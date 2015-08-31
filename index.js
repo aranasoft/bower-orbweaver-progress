bb3aedfd7abdba585ba340af4dba4d3b9c895645
Jay Harris <jay@aranasoft.com>
2015-08-31 14:13:25 -0400

    feat(bower): Create bower package for `orbweaver-progress`

---
+++ index.js
@@ -0,0 +1,2 @@
+require('./orbweaver-progress');
+module.exports = 'orbProgress';
---
+++ orbweaver-progress.js
@@ -0,0 +1,97 @@
+(function (window, angular, undefined) {
+  'use strict';
+
+  var orbweaver = angular.module('orbProgress', ['orbResource']);
+
+  orbweaver.factory("orbRestfulProgressService", ['$q', 'orbProgressService', function ($q, orbRestfulResource, orbProgressService) {
+    var defer = function (fn, params) {
+      params = params || {};
+      var deferred = $q.defer();
+      orbProgressService.start();
+      fn(params,
+        function (response) {
+          orbProgressService.done();
+          deferred.resolve(response);
+        },
+        function (response) {
+          orbProgressService.done();
+          deferred.reject(response);
+        });
+      return deferred.promise;
+    };
+
+    var deferInstance = function (inst, fn, params) {
+      params = params || {};
+      var deferred = $q.defer();
+      orbProgressService.start();
+      inst[fn](params,
+        function (response) {
+          orbProgressService.done();
+          deferred.resolve(response);
+        },
+        function (response) {
+          orbProgressService.done();
+          deferred.reject(response);
+        });
+      return deferred.promise;
+    };
+
+    return function (RestfulResource) {
+      return {
+        empty: function () {
+          return new RestfulResource();
+        },
+        all: function (params) {
+          return defer(RestfulResource.query, params);
+        },
+        find: function (id, params) {
+          params = params || {};
+          params = angular.extend(params, {id: id});
+          return defer(RestfulResource.get, params);
+        },
+        save: function (res, params) {
+          params = params || {};
+          if (res.id) {
+            params = angular.extend(params, {id: res.id});
+            return deferInstance(res, "$update", params);
+          } else {
+            return deferInstance(res, "$create", params);
+          }
+        },
+        'delete': function (res, params) {
+          params = params || {};
+          params = angular.extend(params, {id: res.id});
+          return defer(RestfulResource['delete'], params);
+        }
+      };
+    };
+  }]);
+
+  orbweaver.factory("orbProgressService", function () {
+    var progressCounter = 0;
+
+    function incrementProgressCounter() {
+      if (progressCounter === 0) {
+        NProgress.start();
+      }
+      progressCounter++;
+    }
+
+    function decrementProgressCounter() {
+      progressCounter--;
+      if (progressCounter === 0) {
+        NProgress.done();
+      }
+    }
+
+    return {
+      start: function () {
+        incrementProgressCounter();
+      },
+      done: function () {
+        decrementProgressCounter();
+      }
+    };
+  });
+})(window, window.angular);
+
